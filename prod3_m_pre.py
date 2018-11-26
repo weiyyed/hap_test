13e01147f4a161a36033124dbbd0720efae7acd9
weiyyed <weiyyed@163.com>
2018-11-26 17:06:32 +0800

    添加:获取移动端屏幕长宽

---
+++ prod3_m_pre.py
@@ -0,0 +1,39 @@
+from sweetest.autotest import Autotest
+import sys
+
+
+# 项目名称
+project_name = 'Prod3'
+
+# 单用例调试或挑用例执行
+sheet_name = ['pre-m']
+
+# 项目所有用例执行，*模糊匹配
+#sheet_name = 'hap*'
+
+# 环境配置信息
+# Chrome
+desired_caps = {
+        "deviceName": "weiyongyou",
+        "platformName": "android",
+        "Android": "7.0",
+        "appActivity": "com.hayden.hap.fv.login.ui.SplashActivity",
+        "appPackage": "com.hayden.hap.fv",
+                }
+server_url = 'http://127.0.0.1:4723/wd/hub'
+
+
+# 初始化自动化实例
+sweet = Autotest(project_name, sheet_name, desired_caps, server_url)
+
+# 按条件执行,支持筛选的属性有：'id', 'title', 'designer', 'priority'
+# sweet.fliter(priority='H')
+
+# 执行
+sweet.plan()
+
+#测试报告详细数据
+#print(sweet.report_data)
+
+# 如果是集成到 CI/CD，可以给出退出码
+#sys.exit(sweet.code)
